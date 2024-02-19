b5cc1f9f205b4be84fe2bec5121e0549da2ea74e
venkateshaPrabhu811 <kingvenkat521@gmail.com>
2024-02-20 01:56:39 +0530

    Restructure the project to a complete Node Application

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+    
+})
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,() => {
+    console.log(`Server running on ${PORT}`);
+})
